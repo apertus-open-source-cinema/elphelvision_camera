d07b1f2b30a859c6cd700ba02db4c19db871f795
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2009-07-24 16:03:25 +0000

    multiple parameters can now be set at the same time gpl header added
    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/Apertus/ElphelVision/camera%20www@32 46015a06-04f4-4860-9c33-90e0f15d9639

--- setparam.php
+++ setparam.php
@@ -1,20 +1,50 @@
 <?php
-
-$key = $_GET['key'];
+/*! Copyright (C) 2009 Apertus, All Rights Reserved
+ *! Author : Apertus Team
+ *! Description: php script used to set camera parameters
+ *! called by the java application via http request
+-----------------------------------------------------------------------------**
+ *!
+ *!  This program is free software: you can redistribute it and/or modify
+ *!  it under the terms of the GNU General Public License as published by
+ *!  the Free Software Foundation, either version 3 of the License, or
+ *!  (at your option) any later version.
+ *!
+ *!  This program is distributed in the hope that it will be useful,
+ *!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *!  GNU General Public License for more details.
+ *!
+ *!  You should have received a copy of the GNU General Public License
+ *!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *!
+-----------------------------------------------------------------------------**/
 
 $param = array();
-$param[$_GET['key']] = myval($_GET['value']);
+foreach($_GET as $key => $val) {
+    $param[$key] = convert($val);
+}
 
-print_r($param);
+// set parameter for the next frame
+$set_frame = elphel_set_P_arr ($param, elphel_get_frame()+1);
 
-echo elphel_set_P_arr ($param, elphel_get_frame()+1);
+// debugging
+echo "current frame: ".elphel_get_frame()."\n";
+echo "frame with new parameters: ".$set_frame."\n";
+echo "Setting parameter "; print_r($param); echo "\n";
 
-function myval ($s) {
-	$s = trim($s, "\" ");
-	if(strtoupper(substr($s, 0, 2))=="0X")
-		return intval(hexdec($s));
-	else 
-		return intval($s);
+
+function convert($s) {
+    // clean up
+    $s = trim($s, "\" ");
+   
+    // check if value is in HEX
+    if(strtoupper(substr($s, 0, 2))=="0X")
+        return intval(hexdec($s));
+    else
+        return intval($s);
 }
 
 ?>
+
+
