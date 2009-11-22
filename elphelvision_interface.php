94533089b216e050a953764eb4b3b2c8522d9cf0
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2009-11-22 14:13:35 +0000

    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/Apertus/ElphelVision/camera%20www@55 46015a06-04f4-4860-9c33-90e0f15d9639

--- elphelvision_interface.php
+++ elphelvision_interface.php
@@ -1,4 +1,25 @@
 <?php
+/*!***************************************************************************
+*! FILE NAME  : elphelvision_interface.php
+*! DESCRIPTION: interface for the ElphelVision viewfinder software for Apertus open cinema camera
+*! Copyright (C) 2009 Apertus
+*! -----------------------------------------------------------------------------**
+*!
+*!  This program is free software: you can redistribute it and/or modify
+*!  it under the terms of the GNU General Public License as published by
+*!  the Free Software Foundation, either version 3 of the License, or
+*!  (at your option) any later version.
+*!
+*!  This program is distributed in the hope that it will be useful,
+*!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*!  GNU General Public License for more details.
+*!
+*!  You should have received a copy of the GNU General Public License
+*!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*! -----------------------------------------------------------------------------**
+*!
+*/
 
 $cmd = $_GET['cmd'];
 
@@ -93,11 +114,43 @@ switch ($cmd) {
 		echo "<camogm_fileframeduration>".$camogm_fileframeduration."</camogm_fileframeduration>";
 		break;	
 
+	case "gamma":
+		// get the number (page) of the table currently in use from gamma cache
+		$gammas_file = fopen("/dev/gamma_cache", "r");
+		fseek($gammas_file, 0, SEEK_END);
+		$numberOfEntries = ftell($gammas_file);
+		fclose($gammas_file);
+		$gammaStructure = array();
+		$g_raw = elphel_gamma_get_raw(0);
+		$g_raw_ul = unpack('V*', $g_raw);
+		$gammaStructure["num_locked"] = $g_raw_ul[10];
+		$gammaStructure["locked_col"] =  array ($g_raw_ul[11],$g_raw_ul[12],$g_raw_ul[13],$g_raw_ul[14]);
+		
+		$page = $gammaStructure["locked_col"][0];
+		
+		// read gamma table from the page we just got
+		$g_raw = elphel_gamma_get_raw($page);
+		$g_raw_ul = unpack('V*', $g_raw);
+		$a = 11; // skip all the header bits and just start where the gamma table begins
+		
+		for ($i=0; $i<128; $i++) {
+			$d = $g_raw_ul[$a++];
+			$Y[$i*2] = (int)(($d & 0xffff)/256);
+			$Y[$i*2+1] = (int)((($d>>16) & 0xffff)/256);
+		}
+		echo "<gamma>";
+		for ($j = 0; $j < 256; $j++) {
+			echo "<gamma_".$j.">".$Y[$j]."</gamma_".$j.">";
+		}
+		echo "</gamma>";
+		break;	
+
 	default:
 		echo "<image_width>".elphel_get_P_value(ELPHEL_WOI_WIDTH)."</image_width>\n";
 		echo "<image_height>".elphel_get_P_value(ELPHEL_WOI_HEIGHT)."</image_height>\n";
 		echo "<fps>".elphel_get_P_value(ELPHEL_FP1000S)."</fps>\n";
 		echo "<jpeg_quality>".elphel_get_P_value(ELPHEL_QUALITY)."</jpeg_quality>\n";
+		echo "<exposure>".elphel_get_P_value(ELPHEL_EXPOS)."</exposure>\n";
 		
 		if ($camogm_running)
 			echo "<camogm>running</camogm>";
--- histogram.php
+++ histogram.php
@@ -1,19 +1,69 @@
-<?
+<?php
+/*!***************************************************************************
+*! FILE NAME  : histogram.php
+*! DESCRIPTION: histogram interface for the ElphelVision viewfinder software for Apertus open cinema camera
+*! Copyright (C) 2009 Apertus
+*! -----------------------------------------------------------------------------**
+*!
+*!  This program is free software: you can redistribute it and/or modify
+*!  it under the terms of the GNU General Public License as published by
+*!  the Free Software Foundation, either version 3 of the License, or
+*!  (at your option) any later version.
+*!
+*!  This program is distributed in the hope that it will be useful,
+*!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*!  GNU General Public License for more details.
+*!
+*!  You should have received a copy of the GNU General Public License
+*!  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*! -----------------------------------------------------------------------------**
+*!
+*/
 
 if (!$frame) 
-	$frame=elphel_get_frame()-1;
+	$frame = elphel_get_frame()-1;
 
 $colors = array(0=>"R",1=>"G",2=>"GB",3=>"B");
-$h_arr = elphel_histogram_get(0xfff,$frame);
-$a=0;
+$h_arr = elphel_histogram_get(0xfff, $frame);
 $offset2sum=1024+255; /// last in cumulative histogram for the same color
-//echo "<pre>\n";
-//for ($color=1;$color<4;$color++) {
-	//printf("%s:", $colors[1]);
-	for ($i=0; $i<256; $i++) {
-		printf ("%d;",$h_arr[$a++]);
-	}
-//	printf ("\n");
-//}
-//echo "</pre>\n";
 
+// scale histograms always so the highest peak is not clipping
+$highest_value = 1;
+for ($a=0; $a<768; $a++) {
+	if ($highest_value < $h_arr[$a])
+		$highest_value = $h_arr[$a];
+}
+
+$a=0;
+// RED
+$color = 0;
+//printf("\npercentile for color %s:", $colors[$color]);
+for ($i=0; $i<256; $i++) {
+	printf ("%d;", $h_arr[$a]/$highest_value*256);		
+	//printf ("%d;", $h_arr[$a] / $pixelcount * 100);
+	$a++;
+}
+
+// GREEN
+$color = 1;
+//printf("\npercentile for color %s:", $colors[$color]);
+for ($i=0; $i<256; $i++) {
+	printf ("%d;", $h_arr[$a]/$highest_value*256);		
+	//printf ("%d;", $h_arr[$a] / $pixelcount * 100);
+	$a++;
+}
+
+// skip GB
+$a += 255;
+
+// BLUE
+$color = 3;
+//printf("\npercentile for color %s:", $colors[$color]);
+for ($i=0; $i<256; $i++) {
+	printf ("%d;", $h_arr[$a]/$highest_value*256);		
+	//printf ("%d;", $h_arr[$a] / $pixelcount * 100);
+	$a++;
+}
+
+?>
