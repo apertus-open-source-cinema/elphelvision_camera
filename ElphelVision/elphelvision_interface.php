ebcec4c5ccedcf9e01fb835177c2e2a991b42f07
oneartplease <oneartplease@46015a06-04f4-4860-9c33-90e0f15d9639>
2010-10-13 18:11:07 +0000

    added listing of files option
    git-svn-id: https://elphel.svn.sourceforge.net/svnroot/elphel/Apertus/ElphelVision/camera%20www@187 46015a06-04f4-4860-9c33-90e0f15d9639

--- ElphelVision/elphelvision_interface.php
+++ ElphelVision/elphelvision_interface.php
@@ -124,6 +124,60 @@ switch ($cmd) {
 	case "fileframeduration":
 		echo "<camogm_fileframeduration>".$camogm_fileframeduration."</camogm_fileframeduration>";
 		break;	
+	case "list_files":
+		if (!file_exists('/mnt/flash/html/hdd')) {
+			echo "no webshare found";
+		break;
+		}
+		$dir = $_GET["dir"];
+		
+		if (isset($dir) && ($dir != "") && ($dir != "/./") && ($dir != "/"))  // show "one level up" item if we are not in "home" directory
+		{
+			echo "<file>";
+			echo "<type>updir</type>";
+			echo "<name>..</name>";
+			$file_remove_last_slash = substr($dir, 0, strlen($dir)-1);
+			$file_pos_of_second_last_slash = strrpos($file_remove_last_slash, "/");
+			$up_file = substr($dir, 0, $file_pos_of_second_last_slash+1);
+			while(strpos($up_file, "//")) {
+				$up_file = str_replace("//", "/", $up_file); 
+			}
+			if ($up_file == "")
+				echo "<path>/</path>";
+			else
+				echo "<path>".$up_file."</path>";
+			echo "<size>0</size>";
+			echo "<date>0</date>";
+			echo "</file>";
+		}
+		
+		if ($handle = opendir('/mnt/flash/html/hdd'.$dir)) 
+		{
+			while ($file = readdir($handle))
+			{
+				if ($file != "." && $file != "..")
+				{	
+					echo "<file>";
+					echo "<type>";
+					if (is_dir("/var/hdd/".$dir.$file))
+						echo "dir";
+					else
+						echo $extension = substr($file, strrpos($file, '.')+1, strlen($file)); 
+					echo "</type>";
+					echo "<name>".$file."</name>";
+					echo "<path>".substr($dir, 1).$file."</path>";
+					$size = filesize("/var/hdd/".$dir.$file);
+					echo "<size>".$size."</size>";
+					$date = date ("d M Y H:i:s", filectime("/var/hdd/".$dir.$file));
+					echo "<date>".$date."</date>";
+					echo "</file>";
+				}
+			}
+			closedir($handle);
+		}
+		else
+			echo "no webshare found<br>";
+		break;
 	default:
 		echo "<image_width>".elphel_get_P_value(ELPHEL_WOI_WIDTH)."</image_width>\n";
 		echo "<image_height>".elphel_get_P_value(ELPHEL_WOI_HEIGHT)."</image_height>\n";
